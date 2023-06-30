b2b97bc58d779137a6044a4b6400f6138894ad13
Eugene Fedchuk <fed4chuck@gmail.com>
2023-06-30 03:47:22 +0300

    Reworked tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <ostream>
 #include "ftl.hpp"
@@ -5,57 +6,66 @@
 using namespace ftl;
 using namespace std;
 
-Result<double, str> checked_div(double a, double b) {
-    if (b == 0) {
-        return Err((str)"div by zero");
+struct ResultTest {
+    Result<double, str> checked_div(double a, double b) {
+        if (b == 0) {
+            return Err(str{"div by zero"});
+        }
+        return Ok(a / b);
     }
-    return Ok(a / b);
-}
+    void operator()() {
+        Result<double, str> res = checked_div(5, 2);
+        cout << res << endl;
+        cout << res.map(Some<double>) << endl;
 
-Result<void, const char *> fail() {
-    return Err("fail");
-}
-
-Result<void, const char *> success() {
-    return Ok();
-}
-
-Option<int> some_int() {
-    return Some(5);
-}
-
-struct Foo {
-    Foo() = delete;
-    Foo(int) {}
-    friend ostream &operator<<(ostream &out, Foo &self) {
-        (void)self;
-        return out << "Foo";
+        res = checked_div(5, 0);
+        cout << res << endl;
     }
-    ~Foo() {
-        cout << "deleted Foo" << endl;
+};
+
+struct OptionTest {
+    struct Foo {
+        Foo() = delete;
+        Foo(int) {}
+        friend ostream &operator<<(ostream &out, Foo &self) {
+            (void)self;
+            return out << "Foo";
+        }
+        ~Foo() {
+            cout << "deleted Foo" << endl;
+        }
+    };
+    void operator()() {
+        Option<int> opt = Some(5);
+        cout << opt << endl;
+        cout << opt.map([](int a){ return a * 2; }) << endl;
+        cout << opt.map([](int a){ return Foo(a); }) << endl;
+
+        opt = None();
+        cout << opt << endl;
+        cout << opt.ok_or_else([](){ return "kek"; }) << endl;
+    }
+};
+
+struct OptionRefTest {
+    struct Data { int a; };
+    struct Widget {
+        Option<Data> foo; 
+        Option<Data&> data() {
+            return foo.as_ref();
+        }
+    };
+    void operator()() {
+        Widget bar{ Some(Data{ 5 }) };
+        Option<Data&> data = bar.data();
+        assert(data.unwrap().a == 5);
     }
 };
 
 int main() {
-    auto res = checked_div(5, 2);
-    cout << res << endl;
-    cout << res.map(Some<double>) << endl;
-    /* cout << res.map(Some) << endl; */
-
-    res = checked_div(5, 0);
-    cout << res << endl;
-
-    auto error = fail();
-    cout << error << endl;
-
-    auto opt = some_int();
-    cout << opt << endl;
-    cout << opt.map([](int a){ return a * 2; }) << endl;
-    cout << opt.map([](int a){ return Foo(a); }) << endl;
-
-    opt = None();
-    cout << opt << endl;
-    cout << opt.ok_or_else([](){ return "kek"; }) << endl;
+    ResultTest{}();
+    OptionTest{}();
+    OptionRefTest{}();
 
     return 0;
 }
